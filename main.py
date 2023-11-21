ec859d8bfb2f174313c000d15ebc9cf6ad8c88bd
baaaam771 <jeongiun@naver.com>
2023-11-21 18:16:25 +0900

    feat : unittest func upper
    - upper 함수 유닛 테스트

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import unittest
 
-def funcOne():
-    return 1
 
-class TestFuncOne(unittest.TestCase):
-    def testFunc(self):
-        # 테스트 케이스 정의
-        # 예상되는 값 =1
-        # 실제 함수가 호출되면 1이 나오는가?
-        # 호출된 함수 funcOne ==1 ?
 
-        self.assertEqual(funcOne(), 1)
-        # fail 테스트
-        # self.assertEqual(funcOne(), 0)
+class TestStringUpper(unittest.TestCase):
+    def testUpeer(self):
+        self.assertEqual('abc'.upper(), 'ABC')
+        self.assertEqual('Ab'.upper(), 'AB')
+        self.assertEqual('d'.upper(), 'D')
+        
 
 if __name__ == '__main__':
-    # print("Hi~")
+    # 'foo'.upper()
+    # print('abc'.upper())
     unittest.main(exit=False)
-    # print("Bye~")
+    
